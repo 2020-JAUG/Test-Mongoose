a3b48df1ba4627ce6105962b18c579ac56514a7d
Alejandro Urbina <urbinagonzalezja@gmail.com>
2021-06-03 00:39:27 +0200

    Add routh to posts

--- router.js
+++ router.js
@@ -1,8 +1,10 @@
 const router = require("express").Router();
 const userRouter = require("./routes/userRouter");
 const chatRouter = require("./routes/chatRouter");
+const postRouter = require("./routes/postRouter");
 
 router.use("/user", userRouter);
 router.use("/chat", chatRouter);
+router.use("/post", postRouter);
 
 module.exports = router;
