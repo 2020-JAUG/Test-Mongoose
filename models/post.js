81aaaf5340237b81915908653126e77d39bfc6db
Alejandro Urbina <urbinagonzalezja@gmail.com>
2021-06-03 00:42:07 +0200

    Create the model post

---
+++ models/post.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const postSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    creationDate: {
+        type: Date,
+        required: true
+    },
+    post: {
+        type: Array
+    },
+    isActive: {
+        type: Boolean,
+        default: true
+    }
+});
+
+
+const Post = mongoose.model('Post', postSchema);
+module.exports = Post;
